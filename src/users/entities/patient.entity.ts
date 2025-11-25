03f81b874814afc6bca8d3b7e536445ee2e2bd9f
masihur Rohman <masihur@evidentbd.com>
2025-11-25 19:27:34 +0600

    Register

---
+++ src/users/entities/patient.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('patients')
+export class Patient {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
